33f721261c77b4f8936e7f944f42e074a8924f74
Dilnoza Akhrarova <121368213+dilnoza1984@users.noreply.github.com>
2022-12-29 03:12:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+int z;
+
+    int x = 55;
+    int y = 77;
+
+    z = x = 55;
+    x = y = 77;
+    y = z = 55;
+
+    System.out.println(x + " " + y);
+
+    //swap values 
+
+    
+
+    
+  }
+}
